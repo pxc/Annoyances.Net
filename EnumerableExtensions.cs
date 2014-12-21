69a6fb33be7fd172b8a0719ff7f3bcf77bf61dd2
Matthew Strawbridge <matthew@virtualpairprogrammers.com>
2014-12-21 00:02:52 +0000

    Added Default versions of Min/Max/Average

---
+++ EnumerableExtensions.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Annoyances.Net
+{
+    /// <summary>
+    /// More LINQ-like methods. Extension methods for <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        #region MaxOrDefault
+        public static T MaxOrDefault<T>(this IEnumerable<T> sequence, T defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Max();
+        }
+
+        public static TResult MaxOrDefault<T, TResult>(
+            this IEnumerable<T> sequence, Func<T, TResult> f, TResult defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Max(f);
+        }
+        #endregion
+
+        #region MinOrDefault
+        public static T MinOrDefault<T>(this IEnumerable<T> sequence, T defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Min();
+        }
+
+        public static TResult MinOrDefault<T, TResult>(
+            this IEnumerable<T> sequence, Func<T, TResult> f, TResult defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Min(f);
+        }
+        #endregion
+
+        #region AverageOrDefault_decimal
+        public static decimal AverageOrDefault<T>(this IEnumerable<decimal> sequence, decimal defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average();
+        }
+
+        public static decimal AverageOrDefault<T>(
+            this IEnumerable<T> sequence, Func<T, decimal> f, decimal defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average(f);
+        }
+
+        public static decimal? AverageOrDefault<T>(this IEnumerable<decimal?> sequence, decimal? defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average();
+        }
+
+        public static decimal? AverageOrDefault<T>(
+            this IEnumerable<T> sequence, Func<T, decimal?> f, decimal? defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average(f);
+        }
+        #endregion
+
+        #region AverageOrDefault_double
+        public static double AverageOrDefault<T>(this IEnumerable<double> sequence, double defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average();
+        }
+
+        public static double AverageOrDefault<T>(
+            this IEnumerable<T> sequence, Func<T, double> f, double defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average(f);
+        }
+
+        public static double? AverageOrDefault<T>(this IEnumerable<double?> sequence, double? defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average();
+        }
+
+        public static double? AverageOrDefault<T>(
+            this IEnumerable<T> sequence, Func<T, double?> f, double? defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average(f);
+        }
+        #endregion
+
+        #region AverageOrDefault_float
+        public static float AverageOrDefault<T>(this IEnumerable<float> sequence, float defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average();
+        }
+
+        public static float AverageOrDefault<T>(
+            this IEnumerable<T> sequence, Func<T, float> f, float defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average(f);
+        }
+
+        public static float? AverageOrDefault<T>(this IEnumerable<float?> sequence, float? defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average();
+        }
+
+        public static float? AverageOrDefault<T>(
+            this IEnumerable<T> sequence, Func<T, float?> f, float? defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average(f);
+        }
+        #endregion
+
+        #region AverageOrDefault_int
+        public static double AverageOrDefault<T>(this IEnumerable<int> sequence, double defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average();
+        }
+
+        public static double AverageOrDefault<T>(
+            this IEnumerable<T> sequence, Func<T, int> f, double defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average(f);
+        }
+
+        public static double? AverageOrDefault<T>(this IEnumerable<int?> sequence, double? defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average();
+        }
+
+        public static double? AverageOrDefault<T>(
+            this IEnumerable<T> sequence, Func<T, int?> f, double? defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average(f);
+        }
+        #endregion
+
+        #region AverageOrDefault_long
+        public static double AverageOrDefault<T>(this IEnumerable<long> sequence, double defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average();
+        }
+
+        public static double AverageOrDefault<T>(
+            this IEnumerable<T> sequence, Func<T, long> f, double defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average(f);
+        }
+
+        public static double? AverageOrDefault<T>(this IEnumerable<long?> sequence, double? defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average();
+        }
+
+        public static double? AverageOrDefault<T>(
+            this IEnumerable<T> sequence, Func<T, long?> f, double? defaultValue)
+        {
+            if (sequence == null || !sequence.Any())
+            {
+                return defaultValue;
+            }
+
+            return sequence.Average(f);
+        }
+        #endregion
+    }
+}
---
+++ tests/EnumerableExtensionsTests.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Annoyances.Net.Tests
+{
+    // ReSharper disable ExpressionIsAlwaysNull
+    // ReSharper disable RedundantTypeArgumentsOfMethod
+    [TestFixture]
+    public class EnumerableExtensionsTests
+    {
+        [Test]
+        public void TestMaxOrDefaultWithNullExpectDefault()
+        {
+            IEnumerable<int> sequence = null;
+            Assert.That(sequence.MaxOrDefault(-1), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void TestMaxOrDefaultWithEmptySequenceExpectDefault()
+        {
+            IEnumerable<int> sequence = Enumerable.Empty<int>();
+            Assert.That(sequence.MaxOrDefault(-1), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void TestMaxOrDefaultWithSequenceExpectMax()
+        {
+            IEnumerable<int> sequence = new[] {1, 2, 3, 2};
+            Assert.That(sequence.MaxOrDefault(-1), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void TestMaxOrDefaultTransformWithNullExpectDefault()
+        {
+            IEnumerable<int> sequence = null;
+            Assert.That(sequence.MaxOrDefault(v => v + 1, -1), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void TestMaxOrDefaultTransformWithEmptySequenceExpectDefault()
+        {
+            IEnumerable<int> sequence = Enumerable.Empty<int>();
+            Assert.That(sequence.MaxOrDefault(v => v + 1, -1), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void TestMaxOrDefaultTransformWithSequenceExpectMax()
+        {
+            IEnumerable<int> sequence = new[] { 1, 2, 3, 2 };
+            Assert.That(sequence.MaxOrDefault(v => v + 1, -1), Is.EqualTo(3 + 1));
+        }
+
+        [Test]
+        public void TestMinOrDefaultWithNullExpectDefault()
+        {
+            IEnumerable<int> sequence = null;
+            Assert.That(sequence.MinOrDefault(-1), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void TestMinOrDefaultWithEmptySequenceExpectDefault()
+        {
+            IEnumerable<int> sequence = Enumerable.Empty<int>();
+            Assert.That(sequence.MinOrDefault(-1), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void TestMinOrDefaultWithSequenceExpectMin()
+        {
+            IEnumerable<int> sequence = new[] { 1, 2, 3, 2 };
+            Assert.That(sequence.MinOrDefault(-1), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestMinOrDefaultTransformWithNullExpectDefault()
+        {
+            IEnumerable<int> sequence = null;
+            Assert.That(sequence.MinOrDefault(v => v + 1, -1), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void TestMinOrDefaultTransformWithEmptySequenceExpectDefault()
+        {
+            IEnumerable<int> sequence = Enumerable.Empty<int>();
+            Assert.That(sequence.MinOrDefault(v => v + 1, -1), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void TestMinOrDefaultTransformWithSequenceExpectMin()
+        {
+            IEnumerable<int> sequence = new[] { 1, 2, 3, 2 };
+            Assert.That(sequence.MinOrDefault(v => v + 1, -1), Is.EqualTo(1 + 1));
+        }
+
+        [Test]
+        public void TestAverageOrDefaultWithNullExpectDefault()
+        {
+            IEnumerable<double> sequence = null;
+            Assert.That(sequence.AverageOrDefault<double>(-1.0), Is.EqualTo(-1.0));
+        }
+
+        [Test]
+        public void TestAverageOrDefaultWithEmptySequenceExpectDefault()
+        {
+            IEnumerable<double> sequence = Enumerable.Empty<double>();
+            Assert.That(sequence.AverageOrDefault<double>(-1.0), Is.EqualTo(-1.0));
+        }
+
+        [Test]
+        public void TestAverageOrDefaultWithSequenceExpectAverage()
+        {
+            IEnumerable<double> sequence = new[] { 1.0, 2, 3, 4 };
+            Assert.That(sequence.AverageOrDefault<double>(-1.0), Is.EqualTo(2.5));
+        }
+
+        [Test]
+        public void TestAverageOrDefaultTransformWithNullExpectDefault()
+        {
+            IEnumerable<double> sequence = null;
+            Assert.That(sequence.AverageOrDefault<double>(v => v + 1, -1.0), Is.EqualTo(-1.0));
+        }
+
+        [Test]
+        public void TestAverageOrDefaultTransformWithEmptySequenceExpectDefault()
+        {
+            IEnumerable<double> sequence = Enumerable.Empty<double>();
+            Assert.That(sequence.AverageOrDefault<double>(v => v + 1, -1.0), Is.EqualTo(-1.0));
+        }
+
+        [Test]
+        public void TestAverageOrDefaultTransformWithSequenceExpectAverage()
+        {
+            IEnumerable<double> sequence = new[] { 1.0, 2, 3, 4 };
+            Assert.That(sequence.AverageOrDefault<double>(v => v + 1, -1.0), Is.EqualTo(2.5 + 1));
+        }
+    }
+    // ReSharper restore RedundantTypeArgumentsOfMethod
+    // ReSharper restore ExpressionIsAlwaysNull
+}
