0d137b9e4d3e4155bd4489e482c044c46e0d922c
Matthew Strawbridge <matthew@virtualpairprogrammers.com>
2014-12-21 00:16:27 +0000

    Added case-insensitive string.Contains

---
+++ StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Annoyances.Net
+{
+    /// <summary>
+    /// Extensions to <see cref="String"/>
+    /// </summary>
+    public static class StringExtensions
+    {
+        public static bool Contains(this string s, string substring, CultureInfo culture, CompareOptions options)
+        {
+            return culture.CompareInfo.IndexOf(s, substring, options) >= 0;
+        }
+    }
+}
---
+++ tests/StringExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using NUnit.Framework;
+
+namespace Annoyances.Net.Tests
+{
+    [TestFixture]
+    class StringExtensionsTests
+    {
+        [Test]
+        public static void TestContainsWithMatchingSubstringExpectSuccess()
+        {
+            bool result = "longstring".Contains("string", CultureInfo.InvariantCulture, CompareOptions.None);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public static void TestContainsWithNonMatchingSubstringExpectFailure()
+        {
+            bool result = "longstring".Contains("other", CultureInfo.InvariantCulture, CompareOptions.None);
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public static void TestContainsWithDifferentCaseSubstringExpectSuccess()
+        {
+            bool result = "longstring".Contains("LONG", CultureInfo.InvariantCulture, CompareOptions.IgnoreCase);
+            Assert.That(result, Is.True);
+        }
+
+
+    }
+}
