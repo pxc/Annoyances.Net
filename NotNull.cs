185a412ee73e4058344c794864c72f6644170c53
Matthew Strawbridge <matthew@virtualpairprogrammers.com>
2014-12-21 11:13:34 +0000

    Added NotNull wrapper

---
+++ NotNull.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Annoyances.Net
+{
+    public class NotNull<T> where T : class
+    {
+        public T Value { get; private set; }
+
+        private NotNull(T value)
+        {
+            Debug.Assert(value != null);
+            Value = value;
+        }
+
+        public static NotNull<T> Create(T value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+
+            return new NotNull<T>(value);
+        }
+
+        public static implicit operator T(NotNull<T> value)
+        {
+            return value.Value;
+        }
+    }
+}
---
+++ tests/NotNullTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace Annoyances.Net.Tests
+{
+    [TestFixture]
+    public static class NotNullTests
+    {
+        [Test]
+        public static void TestCreateWithNullExpectException()
+        {
+            Assert.That(() => NotNull<object>.Create(null), Throws.Exception);
+        }
+
+        [Test]
+        public static void TestCreateWithNotNullExpectSuccess()
+        {
+            Assert.That(() => NotNull<object>.Create(new object()), Throws.Nothing);
+        }
+
+        [Test]
+        public static void TestImplicitConversionWithNotNullExpectSuccess()
+        {
+            NotNull<string> nn = NotNull<string>.Create("test");            
+
+            Assert.That(((string)nn).Length, Is.EqualTo(4));
+        }
+
+        [Test]
+        public static void TestValueWithNotNullExpectSuccess()
+        {
+            NotNull<string> nn = NotNull<string>.Create("test");
+
+            Assert.That(nn.Value.Length, Is.EqualTo(4));
+        }
+    }
+}
