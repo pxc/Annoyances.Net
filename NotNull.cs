6d2386a9061ab6a123d34d3ea268f822eca68b20
Matthew Strawbridge <matthew@virtualpairprogrammers.com>
2015-03-01 16:57:32 +0000

    Added a comment to clarify the purpose of NotNull<T>

--- NotNull.cs
+++ NotNull.cs
@@ -3,6 +3,17 @@ using System.Diagnostics;
 
 namespace Annoyances.Net
 {
+    /// <summary>
+    /// Helper class to specify parameters that shouldn't be null
+    /// </summary>
+    /// <typeparam name="T">The type of the parameter</typeparam>
+    /// <remarks>
+    /// This doesn't guarantee that the parameter is non-null.
+    /// For example, a user can do this:
+    /// <c>NotNull<string> p = null;</string></c>
+    /// It just makes it less likely that nulls are passed in by accident
+    /// since the easiest way to get one of these objects is to call <see cref="Create"/>.
+    /// </remarks>
     public sealed class NotNull<T> where T : class
     {
         public T Value { get; private set; }
