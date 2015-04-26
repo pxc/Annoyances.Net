1d0258d4c86581a95c344a168e178b6d331d4d35
Matthew Strawbridge <matthew@virtualpairprogrammers.com>
2015-04-26 17:24:06 +0100

    Added Permutation

---
+++ Permutation.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Annoyances.Net
+{
+    /// <summary>
+    /// Utilities for working with permutations
+    /// </summary>
+    /// <seealso cref="EnumerableExtensions.Permute{T}"/>
+    public static class Permutation
+    {
+        /// <summary>
+        /// Gets the sequence of 0-based index position groups that cover every combination
+        /// of <paramref name="numberToChoose"/> elements out of <paramref name="total"/>
+        /// in strictly ascending order.
+        /// </summary>
+        /// <param name="numberToChoose">The number of elements to pick</param>
+        /// <param name="total">The total number of elements</param>
+        /// <returns>A sequence of arrays. Each array has <paramref name="numberToChoose"/> entries in ascending order. Each entry is a 0-based index.</returns>
+        /// <example>
+        /// GetIndexes(2, 4) = { {0, 1}, {0, 2}, {0, 3}, {1, 2}, {1, 3}, {2, 3} , {2, 4}, {3, 4} }
+        /// which represents the indexes of all distinct pairs from a set of 4 elements.
+        /// </example>
+        public static IEnumerable<int[]> GetIndexes(int numberToChoose, int total)
+        {
+            if (numberToChoose < 0)
+            {
+                throw new ArgumentOutOfRangeException("numberToChoose", numberToChoose, "should be at least zero");
+            }
+
+            if (numberToChoose > total)
+            {
+                throw new ArgumentException("numberToChoose should be <= total", "numberToChoose");
+            }
+
+            if (numberToChoose == 0)
+            {
+                // do nothing
+            }
+            else if (numberToChoose == total)
+            {
+                yield return Enumerable.Range(0, numberToChoose).ToArray();                
+            }
+            else
+            {
+                // start with an array of indexes
+                int[] indexes = Enumerable.Range(0, numberToChoose).ToArray();
+
+                bool hasMore;
+                do
+                {
+                    yield return (int[])indexes.Clone();
+
+                    hasMore = Increment(indexes, total - 1);
+                } while (hasMore);
+            }
+        }
+
+        private static bool Increment(int[] indexes, int maxIndex)
+        {
+            int indexOfRightmostOneWeCanIncrement = indexes.Length - 1;
+
+            do
+            {
+                int maxValue = maxIndex - (indexes.Length - indexOfRightmostOneWeCanIncrement) + 1;
+
+                if (indexes[indexOfRightmostOneWeCanIncrement] != maxValue)
+                {
+                    indexes[indexOfRightmostOneWeCanIncrement]++;
+                    for (int i = indexOfRightmostOneWeCanIncrement + 1; i < indexes.Length; i++)
+                    {
+                        indexes[i] = indexes[i - 1] + 1;
+                    }
+
+                    return true;
+                }
+
+                indexOfRightmostOneWeCanIncrement--;
+            } while (indexOfRightmostOneWeCanIncrement >= 0);
+
+            return false;
+        }
+    }
+}
---
+++ tests/PermutationTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Annoyances.Net.Tests
+{
+    [TestFixture]
+    public static class PermutationTests
+    {
+        [Test]
+        public static void TestGetIndexesWithZerosExpectEmptySequence()
+        {
+            var result = Permutation.GetIndexes(0, 0);
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public static void TestGetIndexesWithZeroCountExpectEmptySequence()
+        {
+            var result = Permutation.GetIndexes(0, 1);
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public static void TestGetIndexesWithCountGreaterThanTotalExpectException()
+        {
+            Assert.That(() => Permutation.GetIndexes(2, 1).ToArray(), Throws.ArgumentException);
+        }
+
+        [Test]
+        public static void TestGetIndexesWithNegativeCountExpectException()
+        {
+            Assert.That(() => Permutation.GetIndexes(-1, 1).ToArray(), Throws.InstanceOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public static void TestGetIndexesWithNegativeTotalExpectException()
+        {
+            Assert.That(() => Permutation.GetIndexes(1, -1).ToArray(), Throws.Exception);
+        }
+
+        [Test]
+        public static void TestGetIndexesWithCountEqualToTotalExpectSuccess([Values(1, 2, 3, 5, 10)]int n)
+        {
+            var expectedResult = new[] {Enumerable.Range(0, n).ToArray()};
+
+            var result = Permutation.GetIndexes(n, n);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public static void TestGetIndexesWithCount1ExpectSuccess([Values(1, 2, 3, 5, 10)]int total)
+        {
+            var expectedResult = Enumerable.Range(0, total).Select(i => new[] {i});
+
+            var result = Permutation.GetIndexes(1, total);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public static void TestGetIndexesWith2And3ExpectSuccess()
+        {
+            var expectedResult = new[] {new[] {0, 1}, new[] {0, 2}, new[] {1, 2}};
+
+            var result = Permutation.GetIndexes(2, 3);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public static void TestGetIndexesWith3And5ExpectSuccess()
+        {
+            var expectedResult = new[]
+                {
+                    new[] {0, 1, 2}, new[] {0, 1, 3}, new[] {0, 1, 4}, new[] {0, 2, 3}, new[] {0, 2, 4},
+                    new[] {0, 3, 4}, new[] {1, 2, 3}, new[] {1, 2, 4}, new[] {1, 3, 4}, new[] {2, 3, 4}
+                };
+
+            var result = Permutation.GetIndexes(3, 5);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public static void TestGetIndexesWithARealExampleExpectSuccess()
+        {
+            // suppose we have a mathematics problem where we have to use exactly three of the
+            // four arithmetic operators
+
+            var operators = new[] {'+', '-', '*', '/'};
+
+            var expectedSubsets = new[]
+                {
+                    new[] {'+', '-', '*'}, new[] {'+', '-', '/'}, new[] {'+', '*', '/'},
+                    new[] {'-', '*', '/'}
+                };
+
+            var indexes = Permutation.GetIndexes(3, operators.Length);
+
+            var subsets = indexes.Select(i => new[] {operators[i[0]], operators[i[1]], operators[i[2]]});
+
+            Assert.That(subsets, Is.EqualTo(expectedSubsets));
+
+            // If you want all possible orders of each result then you can do this
+            // (using EnumerableExtensions.Permute):
+            var allPermutations = subsets.SelectMany(s => s.Permute());
+            Assert.That(allPermutations.Count(), Is.EqualTo(24));
+        }
+    }
+}
