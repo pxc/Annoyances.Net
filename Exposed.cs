66e10a68edbafad9627889fc990a3d1a7687b567
Matthew Strawbridge <matthew@virtualpairprogrammers.com>
2015-05-08 23:16:14 +0100

    Added Exposed

---
+++ Exposed.cs
@@ -0,0 +1,109 @@
+﻿using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+
+namespace Annoyances.Net
+{
+    /// <summary>
+    /// A dangerous class for exposing the contents of other classes without having to go fishing with reflection directly.
+    /// Don't use it. No, seriously. Pretend you never saw it. Go back about your business.
+    /// Ah well, okay then. But please be careful!
+    /// <example>
+    /// <code>
+    /// dynamic e = new Exposed(someClass);
+    /// e.PrivateProperty = 123;
+    /// e.DoDangerousThing();
+    /// </code>
+    /// </example>
+    /// </summary>
+    public class Exposed : DynamicObject
+    {
+        /// <summary>
+        /// The object being exposed
+        /// </summary>
+        private readonly object m_o;
+
+        /// <summary>
+        /// Bind promiscuously
+        /// </summary>
+        private const BindingFlags AllBindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+
+        public Exposed(object o)
+        {
+            m_o = o;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            PropertyInfo pi = m_o.GetType().GetProperties(AllBindingFlags).SingleOrDefault(m => m.Name.Equals(binder.Name));
+
+            if (pi == null)
+            {
+                result = "Invalid Property!";
+                return false;
+            }
+
+            result = pi.GetGetMethod(true).Invoke(m_o, new object[0]);
+
+            return true;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            PropertyInfo pi = m_o.GetType().GetProperties(AllBindingFlags).SingleOrDefault(m => m.Name.Equals(binder.Name));
+
+            if (pi == null)
+            {
+                return false;
+            }
+
+            pi.GetSetMethod(true).Invoke(m_o, new[] { value });
+
+            return true;
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            MethodInfo mi = m_o.GetType().GetMethods(AllBindingFlags).SingleOrDefault(m => m.Name.Equals(binder.Name));
+
+            if (mi == null)
+            {
+                result = null;
+                return false;
+            }
+
+            result = mi.Invoke(m_o, args);
+            return true;
+        }
+
+        public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
+        {
+            PropertyInfo pi = m_o.GetType().GetProperties(AllBindingFlags).SingleOrDefault(m => m.GetIndexParameters().Length > 0);
+
+            if (pi == null)
+            {
+                result = "Invalid Indexer!";
+                return false;
+            }
+
+            result = pi.GetGetMethod(true).Invoke(m_o, indexes);
+
+            return true;
+        }
+
+        public override bool TrySetIndex(SetIndexBinder binder, object[] indexes, object value)
+        {
+            PropertyInfo pi = m_o.GetType().GetProperties(AllBindingFlags).SingleOrDefault(m => m.GetIndexParameters().Length > 0);
+
+            if (pi == null)
+            {
+                return false;
+            }
+
+            pi.SetValue(m_o, value, indexes);
+
+            return true;
+        }
+    }
+}
+
---
+++ tests/ExposedTests.cs
@@ -0,0 +1,137 @@
+﻿using NUnit.Framework;
+
+namespace Annoyances.Net.Tests
+{
+    /// <summary>
+    /// A class to test <seealso cref="Exposed"/> with.
+    /// </summary>
+    public class SomeClass
+    {
+        private bool PrivateGetter
+        {
+            get { return true; }
+        }
+
+        protected bool ProtectedGetter
+        {
+            get { return true; }
+        }
+
+        public bool PublicGetter
+        {
+            get { return true; }
+        }
+
+        private bool PrivateProperty { get; set; }
+
+        protected bool ProtectedProperty { get; set; }
+
+        public bool PublicProperty { get; set; }
+
+        private bool PrivateMethod()
+        {
+            return true;
+        }
+
+        protected bool ProtectedMethod()
+        {
+            return true;
+        }
+
+        public bool PublicMethod()
+        {
+            return true;
+        }
+
+        private bool this[int index]
+        {
+            get { return true; }
+            set { }
+        }
+    }
+
+    [TestFixture]
+    public class ExposedTests
+    {
+        /// <summary>
+        /// An exposed version of <see cref="SomeClass"/>, reset before each test.
+        /// </summary>
+        private dynamic m_exposed;
+
+        [SetUp]
+        public void SetUp()
+        {
+            m_exposed = new Exposed(new SomeClass());
+        }
+
+        [Test]
+        public void TestExposedWithPrivateGetterExpectAccess()
+        {
+            Assert.That(m_exposed.PrivateGetter, Is.True);
+        }
+
+        [Test]
+        public void TestExposedWithProtectedGetterExpectAccess()
+        {
+            Assert.That(m_exposed.ProtectedGetter, Is.True);
+        }
+
+        [Test]
+        public void TestExposedWithPublicGetterExpectAccess()
+        {
+            Assert.That(m_exposed.PublicGetter, Is.True);
+        }
+
+        [Test]
+        public void TestExposedWithPrivatePropertyExpectAccess()
+        {
+            m_exposed.PrivateProperty = true;
+            Assert.That(m_exposed.PrivateProperty, Is.True);
+        }
+
+        [Test]
+        public void TestExposedWithProtectedPropertyExpectAccess()
+        {
+            m_exposed.ProtectedProperty = true;
+            Assert.That(m_exposed.ProtectedProperty, Is.True);
+        }
+
+        [Test]
+        public void TestExposedWithPublicPropertyExpectAccess()
+        {
+            m_exposed.PublicProperty = true;
+            Assert.That(m_exposed.PublicProperty, Is.True);
+        }
+
+        [Test]
+        public void TestExposedWithPrivateMethodExpectAccess()
+        {
+            Assert.That(m_exposed.PrivateMethod(), Is.True);
+        }
+
+        [Test]
+        public void TestExposedWithProtectedMethodExpectAccess()
+        {
+            Assert.That(m_exposed.ProtectedMethod(), Is.True);
+        }
+
+        [Test]
+        public void TestExposedWithPublicMethodExpectAccess()
+        {
+            Assert.That(m_exposed.PublicMethod(), Is.True);
+        }
+
+        [Test]
+        public void TestExposedWithPrivateIndexerGetterExpectAccess()
+        {
+            Assert.That(m_exposed[0], Is.True);
+        }
+
+        [Test]
+        public void TestExposedWithPrivateIndexerSetterExpectAccess()
+        {
+            Assert.That(() => m_exposed[0] = true, Throws.Nothing);
+        }
+    }
+}
+
